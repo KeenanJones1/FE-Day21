9f760dbf9125ad1f9ab613a7ba770d4d493fd3b0
Keenan Jones <keenanjones210@gmail.com>
2020-07-17 19:29:38 -0500

    Dailies moving through props

---
+++ src/components/Daily/Dallies.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Dallies = (props) => {
+ console.log(props.daily.habits)
+ return (
+  <div>
+   
+  </div>
+ )
+}
+
+export default Dallies
---
+++ src/components/Title.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Typography} from '@material-ui/core'
+
+const Title = () => {
+ return (
+  <div id="title-container">
+   <Typography variant="h6">Welcome to </Typography>
+        <Typography variant="h1" color="primary" id="title">Day21</Typography>
+        <Typography variant="h5">Keep track of your Daily Habits</Typography>
+  </div>
+ )
+}
+
+export default Title
